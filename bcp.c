8b7c231b6f85f48fc94855b58c3ca98e1dbc5f9e
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-02-20 18:57:41 +0530

    BCP C Source v2.3
    1. Changed the usage of ftell() to more robust conditional compilation that gets 64-bit file offsets on all Windows and *NIXES, and if it does not detect either envirnment + detects a 32-bit long , user is warned and allowed to abort. Thanks to u/oh5nxo for pointing that out : https://www.reddit.com/r/C_Programming/comments/lnezvj/bytecopy_simpler_performant_alternative_within_1/go06sav?utm_source=share&utm_medium=web2x&context=3
    2. Changed approx() to a function-like macro.
    3. Removed usage of number of blocks and switched back to number of bytes to keep track to progress and such. 
    4. Changed slightly how progress is printed to make it more readable and fix the fact that we were wrongly checking for % increase bing >= 1 to print. 
        Also removes the limitation of printing only if filesize if BLOCK*100 , now minimu filesize to print % is 50 bytes, which is too low to be a real restriction anyways.
    5. Comment & variable nam changes as appropriate.

--- bcp.c
+++ bcp.c
@@ -1,7 +1,8 @@
-/* Two headers will be needed, standard I/O and standard integer
+/* Three headers will be needed : standard I/O, standard integer and limits
  * 
  * stdio.h provides the I/O functions : printf(), perror(), FILE*, fseek(), ftell() OR _ftelli64(), fopen(), fclose(), fread(), fwrite(), fflush()
  * stdint.h provides uint_fast8_t , uint_fast64_t
+ * limits.h allows us to check if a long is at least 64-bit and act accordingly
  *
  * Two macros help easily configure the program.
  *
@@ -12,21 +13,17 @@
 */
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
 
 #define PRINT_PROGRESS
 #define BLOCK (1*1048576)
 
-//Trivial function that rounds any +ve floating point type number to an integer type
-uint_fast64_t approx(long double num)
-{
-	return (uint_fast64_t)(num+0.5);
-}
+#define approx(num) ((uint_fast64_t)(num+0.5)) //Trivial macro rounds +ve floating point number to nearest integer type. 
 
 int main(int argc, char * argv[])
 {
-	//usage : bcp [source] [destination]
 
-	if(argc >= 3)
+	if(argc >= 3) //usage : bcp [source] [destination]
 	{
 		/* Open source and destination files as binary streams to read and write to.
 		 * Proceed if both streams were successfully opened.
@@ -39,23 +36,63 @@ int main(int argc, char * argv[])
 		{
 			/* Get the size of the file in bytes.
 			 *
-			 * fseek to end, find the offset from the begining.This offset is the size , in bytes, of the file.
-			 * Since filesize can't be negative, save to unsigned 64-bit integer.
-			 * Rewind the stream to it's begining.
+			 * In short : 
+			 * fseek() to end, find the offset from the begining. This offset is the size , in bytes, of the file. Rewind the stream to it's begining.
 			 *
-			 * Why the ifdef _WIN32 and _ftelli64() ?
-			 *
-			 * Per standard , ftell() returns a LONG INT. 
-			 * This is 64-bit signed value in *NIX systems, which is ~8 exabytes,i.e. sufficient.
-			 * This is a 32-bit signed value in NT systems, which is ~2 gigabytes,i.e. insufficient. 
-			 * Thus,on Windows, use _ftelli64(), a "microsoft function" returning a 64-bit signed value
+			 * What's with the conditional compilation below ?
+			 * 
+			 * For us to use files sizes larger than  ~2GB, we need to be able to represent the number of bytes in it in a 64-bit number.
+			 * The standard C function ftell() returns a long, which can be 32-bit (Windows, 32-bit BSDs & Linux, etc.), and the standard guarantees little. 
+			 * So, we :
+			 * 
+			 * 1. Use ftell() directly if a long is 64-bit. (LONG_MAX == LLONG_MAX, implies sizeof(long) == sizeof(long long), implies long is at least 64 bit)
+			 * 2. If long is less than 8 bytes, this is likely due to being on windows. Are we on windows ? The _WIN32 macro's absence or presence
+			 *    will tell us. If we are on windows, we'll use microsoft's _ftelli64() which returns __int64 (64-bit signed value).
+			 * 3. If we're not on windows, this might be a 32-bit *NIX system.The __unix__ , __unix, unix or __APPLE__ and __MACH__ (macOS) macros will tell us.
+			 *    POSIX standard defines ftello(), similar to ftell(), but return off_t instead of long.
+			 *    POSIX also specifies that if we define _FILE_OFFSET_BITS to be 64, off_t is a 64-bit value. So, we'll do that.
+			 * 4. In the unlikely case that this is a non-*NIX non-Windows environment with longs less than 64-bits, we'll need to inform the user explicitly and ask
+			 *    if they wish to abort, and warn that BCP won't copy more than 2GB in such cases. Since we can't guess on behalf of the user in such matters, we'll force them
+			 *    into an infinite loop asking until they explicitly tell us. 
+			 * 
 			 */
 
 			fseek(from,0,SEEK_END);
-			#ifdef _WIN32
-			uint_fast64_t bytes = _ftelli64(from);
+			
+			#if LONG_MAX == LLONG_MAX
+			long bytes = ftell(from);
+			
+			#elif defined _WIN32
+			__int64 = _ftelli64(from);
+			
+			#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+			#define _FILE_OFFSET_BITS 64
+			off_t bytes = ftello(from);
+			
 			#else
-			uint_fast64_t bytes = ftell(from);
+			puts("WARNING : BCP could not use 64-bit file offsets.\nIf your file is larger than or close to 2 GB in size, abort immediately !");
+			while(1)
+			{
+				char abort[101] = "";
+				
+				printf("Abort ? (Y/N) : ");
+				fgets(abort,101,stdin);
+			
+				if(*abort == 'n' || *abort == 'N')
+					break;
+				else if(*abort == 'y' || *abort == 'Y')
+				{
+					puts("Aborting...");
+					fclose(from);
+					fclose(to);
+					return 1;
+				}
+				else
+					continue;
+			}
+			
+			long bytes = ftell(from);
+			
 			#endif
 
 			rewind(from);
@@ -67,38 +104,37 @@ int main(int argc, char * argv[])
 			 * likely perform better.
 			 *
 			 * Also need a way to store bytes processed, for progress and success message.
-			 * Number of blocks copied is more managable and maybe more effecient than storing the number of bytes directly.
+			 * Previous versions stored number of blocks processed, but now BCP uses bytes processed directly for simplicity. 
 			 */
 
 			uint_fast8_t buffer[BLOCK];
-			uint_fast64_t blocks_processed = 0;
+			uint_fast64_t bytes_processed = 0;
 			uint_fast64_t bytes_read;
 
 			#ifdef PRINT_PROGRESS
 			/* How progress is printed efficiently :
 			 *
-			 * 1. Take an approximate value of the number of blocks in in 1% of the source file. Printing only happens if 1% is approximately *at least* one block.
-			 * 2. Every time a block is copied, if net data copied is >= 1% of file size, print the changed percentage. 
+			 * 1. Take an approximate int value of the number of bytes in 1% of the source file.
+			 *     NOTE : printing happens if file size >= 50 bytes; 1% of it has be at least 0.5 - Any lesser and it approximates to 0. 
+			 * 2. Every time 1% or more is copied, print the changed percentage. 
 			 *
-			 * That is, we print only 100 times or lesser, and most looped math happens only on integers, which is more efficient.
-			 * Also, on a file smaller than (100 * BLOCK) bytes , 100 MiB for a 1 MiB block or 100x bytes for any x bytes per block, there is no printing whatsoever,
-			 * as that would anyways be unnecessary given modern I/O speeds. 
+			 * That is, we print only 100 times or lesser, and looped math happens only on integers, which is efficient.
 			 *
-			 * So, we need a variable storing the approximate number of blocks in 1% of filesize, and a variable to store percentage.
-			 * As maximum % is 100 and % stays +ve, percent_till_now need not be larger than 8-bits and can be unsigned.
+			 * So, we need a variable storing the approximate number of bytes in 1% of filesize, a variable to store percentage, and another to store new percentage,
+			 * so as to get the change in percentage between iterations.
+			 * 
+			 * As maximum % is 100 and % stays +ve, percentage variables need not be larger than 8-bits and can be unsigned.
 			 */
-			uint_fast64_t one_percent = approx((0.01 * bytes)/BLOCK);
-			uint_fast8_t percent_till_now;
+			uint_fast64_t one_percent = approx((0.01 * bytes));
+			uint_fast8_t percent = 0, percent_now;
 			#endif
 
-			/* Try to fread BLOCK bytes from source stream.
+			/* Try to fread BLOCK bytes from source stream, until we can't.
 			 *
-			 * Loop over the stream, fwrite'ing to destination and updating progress while able to read BLOCK bytes.
 			 * In the loop condition , we are testing *both* fread() & fwrite(). With && operator, if fread() condition fails, it will not attempt to check
-			 * the second condition, hence fwrite() will not be executed with garbage/leftover values if fread() fails, 
-			 * without having to do a less elegant nested conditional underneath the loop.
+			 * the second condition, hence fwrite() will not be executed with garbage/leftover values , without having to do a less elegant nested conditional underneath the loop.
 			 * 
-			 * When unable to read BLOCK bytes, check for any errors. Check BOTH streams, as error *may* be both ways. 
+			 * When unable to read BLOCK bytes, check for any errors. Check BOTH streams rather than an if-else, as error *may* be both ways. 
 			 *
 			 * If there are no errors, we have encountered the last few bytes that are less than BLOCK (by default, 1 MiB). 
 			 * This means we can fwrite these last few bytes and attempt to fclose the streams.
@@ -111,20 +147,24 @@ int main(int argc, char * argv[])
 			 *
 			 * Also it's quite likely , depending on what caused the error, that remove() will also uselessly fail until the user fixes things.
 			 *
-			 * However, we *will* tidy up with fclose'ing the streams - but only after the error message, in case that segfaults
-			 * (which it may, again depending on cause of error)
+			 * However, we *will* tidy up with fclose'ing the streams - but only after the error message, in case that segfaults (which it may, again depending on cause of error)
 			 */
 
 			while((bytes_read = fread(buffer,1,BLOCK,from)) == BLOCK && fwrite(buffer,1,BLOCK,to) == BLOCK)
 			{
+				bytes_processed += BLOCK; // Increment the count of bytes copied by the blocksize.
+				
 				#ifdef PRINT_PROGRESS
-				if(one_percent && (percent_till_now = approx((++blocks_processed) / one_percent)) >= 1)
+				if(!one_percent) continue; // Avoids ZeroDivisionError in case one_percentage bytes rounded off to 0
+				
+				percent_now = bytes_processed / one_percent; // get the new percentage (in int form, since both operands are ints)
+				
+				if((percent_now - percent) >= 1) // avoid wasteful and inefficient printing
 				{
-					printf("%d%%\r", percent_till_now);
+					printf("%u%%\r", percent_now);
 					fflush(stdout);
 				}
-				#else
-				++blocks_processed;
+				percent = percent_now; // update percentage count
 				#endif
 			}
 
@@ -136,7 +176,7 @@ int main(int argc, char * argv[])
 					failed_fwrite : // Used if last fwrite() fails ; see lines 151 to 155 
 					fprintf(stderr,"Failed : Unknown fatal error writing to %s\n",argv[2]);
 				
-				fprintf(stderr,"Forced to abandon copying at %llu bytes... exiting...\n",(long long unsigned)(blocks_processed*BLOCK));
+				fprintf(stderr,"Forced to abandon copying at %llu bytes... exiting...\n",(long long unsigned)(bytes_processed));
 
 				fclose(from);
 				fclose(to);
@@ -154,7 +194,7 @@ int main(int argc, char * argv[])
 				 * Note : failed_fwrite ends with "return -1;" ; no infinite goto bug.
 				 */
 
-				if(bytes_read != 0 && fwrite(buffer,1,bytes_read,to) != bytes_read)
+				if(bytes_read && fwrite(buffer,1,bytes_read,to) != bytes_read)
 					goto failed_fwrite; 
 
 				/* Done copying streams,no errors yet, attempt fclose'ing them.
@@ -170,7 +210,8 @@ int main(int argc, char * argv[])
 
 				if(fclose(to) == 0)
 				{	
-					printf("Copied %llu bytes from %s to %s.\n",(long long unsigned)(blocks_processed*BLOCK)+bytes_read,argv[1],argv[2]);
+					printf("Copied %llu bytes from %s to %s.\n",(long long unsigned)(bytes_processed + bytes_read),argv[1],argv[2]); 
+					// must add in bytes_read from the last fread() when displaying net bytes copied.
 					return 0;
 				}
 				else
@@ -207,5 +248,6 @@ int main(int argc, char * argv[])
 	 * -2 : Error writing to destination (when fclose'ing)
 	 * -3 : Error opening destination/source
 	 * -4 : Insufficient arguments
+	 * 1 : User aborted when informed about non-64 bit file offset in non-*NIX non-WIN environment.
 	 */
 }
