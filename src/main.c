0c8b0ab724e665904e24451e74cdbbdd495fb0f3
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-07-02 13:02:02 +0530

    v3.7
    Minor changes :
    1. Small style changes in code and comments
    2. Changed exit mechanism from return to exit()
    3. Did away with unnecessary fcloses that aren't checked for errors.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 
-/* Usage : bcp [source] [destination (optional)] [overwrite ? (y/n) (optional)]
+/* Usage : bcp <source> [destination] [overwrite ? (y/n) ]
  *
  * Flow of control :
  * 1. Run if at least one argument is given. Otherwise, print and exit with -5.
@@ -10,14 +10,14 @@
  * 	a) If yes, act accordingly. If allowed to overwrite, skip to copying. Else, print error and terminate with -1 if file exists.
  *	b) If no, check if the file exists, if it does, ask for overwrite confirmation and if given, proceed. If not given, print and exit with -2.
  * 3. Open FILE streams. If an output file was given , open it, otherwise write to stdout.
- * 4. If streams opened successfully, call fbcp() on them. Else, print error message and return -4.
+ * 4. If streams opened successfully, call fbcp() on them. Else, print error message and exit with -4.
  * 5. Check return value of fbcp(), and, if given an output file, of its fclose().
- * 6. If both were successful, print success message and return 0.
+ * 6. If both were successful, print success message and exit with 0.
  *    Else, if fbcp() has a non-zero return, return that same value (fbcp() handles printing of any errors internally).
- *    Else, (fclose was unsuccessful), print error message and return -3.
+ *    Else, (fclose was unsuccessful), print error message and exit with -3.
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char **argv)
 {
 
 	if(argc >= 2)
@@ -33,76 +33,53 @@ int main(int argc, char * argv[])
 				else if(!strcmp(argv[3],"n") || !strcmp(argv[3], "no"))
 				{
 					FILE * tmp = fopen(argv[2],"rb");
-					if(tmp != NULL)
+					if(tmp)
 					{
-						fclose(tmp);
 						fprintf(stderr,"%s already exists, aborting...\n",argv[2]);
-						return -1;
+						exit(-1);
 					}
-					fclose(tmp);
 				}
-				else
-					goto ask;
+				else goto ask;
 			}
-			else
-			{
-				ask :
-				if(!overwrite_chk(argv[2]))
-					;
-				else
-					return -2;
-			}
-
+			else ask : if(overwrite_chk(argv[2])) exit(-2);
 		}
 			
 		FILE * from = fopen(argv[1],"rb");
-		FILE * to = stdout;
+		FILE * to = outfile_given? fopen(argv[2],"wb") : stdout;
 		
-		if (outfile_given)
-			to = fopen(argv[2],"wb");
-		
-		if(from != NULL && to != NULL)
+		if(from && to)
 		{
 			int rval = fbcp(from,to);
-			fclose(from);
-
 			int rval2 = 0;
+			
 			if(outfile_given)
 				rval2 = fclose(to);
 			
 			if(!rval && !rval2)
 			{
 				fprintf(stderr,"Copied %llu bytes from %s to %s.\n",(long long unsigned)(bytes_processed + bytes_read),argv[1],(outfile_given)? argv[2] : "stdout");
-				return 0;
+				exit(0);
 			}
 			else if(!rval2)
-				return rval;
+				exit(rval);
 			else
 			{
 				perror("Failed : Error writing to destination ");
-				return -3;
+				exit(-3);
 			}
 		}
 		else
 		{
-			if(from != NULL)
-			{
+			if(from)
 				perror("Failed : Error opening destination ");
-				fclose(from);
-			}
-			if(to != NULL)
-			{
+			if(to)
 				perror("Failed : Error opening source ");
-				
-				if(outfile_given)
-					fclose(to);
-			}
-			return -4;
+			exit(-4);
 		}
 	}
 	else
 	{
 		fprintf(stderr,"Failed : Got 0 arguments, expected at least 1.\n");
-		return -5;
+		exit(-5);
 	}
 }
